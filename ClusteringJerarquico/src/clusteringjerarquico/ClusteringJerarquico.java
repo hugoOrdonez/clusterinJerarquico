3c8d6fd06f7cfa89a434bf117c406bf7be334f3f
hugo <hugo@ResearchTux>
2017-06-08 15:16:12 -0500

    primera hoja

--- ClusteringJerarquico/src/clusteringjerarquico/ClusteringJerarquico.java
+++ ClusteringJerarquico/src/clusteringjerarquico/ClusteringJerarquico.java
@@ -5,6 +5,8 @@
  */
 package clusteringjerarquico;
 
+import com.indexing.org.QueryBp;
+
 /**
  *
  * @author hugo
@@ -14,8 +16,10 @@ public class ClusteringJerarquico {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
+    QueryBp queryBP = new QueryBp();
+    
+    
+    
     
 }
